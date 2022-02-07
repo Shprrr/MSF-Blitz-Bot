48e9a8f8a24975ee5c2a516e93167ad49bde2876
Maxime Talon <maximetalon@hotmail.com>
2022-02-06 23:31:54 -0500

    5 more unit tests

--- Tests/BlitzPageTest.cs
+++ Tests/BlitzPageTest.cs
@@ -68,6 +68,121 @@ namespace Tests
         }
 
         [TestMethod]
+        public void GetData_StryfeIsntBlackPantherInOpponent3()
+        {
+            BlitzPage page = new();
+            var image = (Bitmap)Image.FromFile(Path.Combine(ImageFolder, "ScreenshotGame Stryfe en Black Panther slot 4.png"));
+            Emulator.GameImage.Initialize(image);
+            page.SetImage(Emulator.GameImage);
+
+            var data = page.GetData();
+
+            Assert.IsTrue(data.HasOpponent);
+            AssertHeroData("KreeTank_Counter", 17595, data.PlayerHeroes[0]);
+            AssertHeroData("Nobu", 7330, data.PlayerHeroes[1]);
+            AssertHeroData("KreeDmg_Speed", 13310, data.PlayerHeroes[2]);
+            AssertHeroData("KreeSupport_HoT", 15771, data.PlayerHeroes[3]);
+            AssertHeroData("KreeControl_Assist", 13408, data.PlayerHeroes[4]);
+            AssertHeroData("Wolverine", 18690, data.OpponentHeroes[0]);
+            AssertHeroData("Mystique", 8386, data.OpponentHeroes[1]);
+            AssertHeroData("Sabretooth", 15740, data.OpponentHeroes[2]);
+            AssertHeroData("Stryfe", 15120, data.OpponentHeroes[3]);
+            AssertHeroData("MrSinister", 18465, data.OpponentHeroes[4]);
+        }
+
+        [TestMethod]
+        public void GetData_RescueIsntBlackPantherInOpponent4()
+        {
+            BlitzPage page = new();
+            var image = (Bitmap)Image.FromFile(Path.Combine(ImageFolder, "ScreenshotGame Rescue en Black Panther.png"));
+            Emulator.GameImage.Initialize(image);
+            page.SetImage(Emulator.GameImage);
+
+            var data = page.GetData();
+
+            Assert.IsTrue(data.HasOpponent);
+            AssertHeroData("ScientistSupreme", 413, data.PlayerHeroes[0]);
+            AssertHeroData("AimDmg_Speed", 382, data.PlayerHeroes[1]);
+            AssertHeroData("AimDmg_Offense", 446, data.PlayerHeroes[2]);
+            AssertHeroData("Graviton", 414, data.PlayerHeroes[3]);
+            AssertHeroData("AimTank_Taunt", 381, data.PlayerHeroes[4]);
+            AssertHeroData("Thanos", 528, data.OpponentHeroes[0]);
+            AssertHeroData("Carnage", 575, data.OpponentHeroes[1]);
+            AssertHeroData("SpiderMan", 370, data.OpponentHeroes[2]);
+            AssertHeroData("IronMan", 485, data.OpponentHeroes[3]);
+            AssertHeroData("Rescue", 484, data.OpponentHeroes[4]);
+        }
+
+        [TestMethod]
+        public void GetData_BlackBoltIsntBlackPantherInOpponent4()
+        {
+            BlitzPage page = new();
+            var image = (Bitmap)Image.FromFile(Path.Combine(ImageFolder, "ScreenshotGame Black Bolt en Black Panther.png"));
+            Emulator.GameImage.Initialize(image);
+            page.SetImage(Emulator.GameImage);
+
+            var data = page.GetData();
+
+            Assert.IsTrue(data.HasOpponent);
+            AssertHeroData("SilverSamurai", 415, data.PlayerHeroes[0]);
+            AssertHeroData("HandDmg_Bonus", 381, data.PlayerHeroes[1]);
+            AssertHeroData("AimControl_Infect", 446, data.PlayerHeroes[2]);
+            AssertHeroData("LadyDeathstrike", 383, data.PlayerHeroes[3]);
+            AssertHeroData("HandDmg_Unbuff", 287, data.PlayerHeroes[4]);
+            AssertHeroData("CaptainMarvel", 346, data.OpponentHeroes[0]);
+            AssertHeroData("InvisibleWoman", 533, data.OpponentHeroes[1]);
+            AssertHeroData("Phoenix", 452, data.OpponentHeroes[2]);
+            AssertHeroData("Minerva", 491, data.OpponentHeroes[3]);
+            AssertHeroData("BlackBolt", 453, data.OpponentHeroes[4]);
+        }
+
+        [TestMethod]
+        public void GetData_KestrelIsntX23InOpponent3()
+        {
+            BlitzPage page = new();
+            var image = (Bitmap)Image.FromFile(Path.Combine(ImageFolder, "ScreenshotGame Kestrel en X-23 slot 4.png"));
+            Emulator.GameImage.Initialize(image);
+            page.SetImage(Emulator.GameImage);
+
+            var data = page.GetData();
+
+            Assert.IsTrue(data.HasOpponent);
+            AssertHeroData("HandControl_HealBlock", 414, data.PlayerHeroes[0]);
+            AssertHeroData("Cloak", 20115, data.PlayerHeroes[1]);
+            AssertHeroData("HandSupport_Heal", 2595, data.PlayerHeroes[2]);
+            AssertHeroData("Dagger", 20573, data.PlayerHeroes[3]);
+            AssertHeroData("Deathpool", 383, data.PlayerHeroes[4]);
+            AssertHeroData("Gamora", 454, data.OpponentHeroes[0]);
+            AssertHeroData("CaptainMarvel", 21784, data.OpponentHeroes[1]);
+            AssertHeroData("Thor", 20836, data.OpponentHeroes[2]);
+            AssertHeroData("Sybil", 529, data.OpponentHeroes[3]);
+            AssertHeroData("BaronMordo", 2493, data.OpponentHeroes[4]);
+        }
+
+        [TestMethod]
+        public void GetData_ToadIsntWinterSoldierInOpponent4()
+        {
+            BlitzPage page = new();
+            var image = (Bitmap)Image.FromFile(Path.Combine(ImageFolder, "ScreenshotGame Toad en Winter Soldier.png"));
+            Emulator.GameImage.Initialize(image);
+            page.SetImage(Emulator.GameImage);
+
+            var data = page.GetData();
+
+            Assert.IsTrue(data.HasOpponent);
+            AssertHeroData("HydraTank_Taunt", 195, data.PlayerHeroes[0]);
+            AssertHeroData("HydraDmg_Single", 476, data.PlayerHeroes[1]);
+            AssertHeroData("HydraDmg_Buff", 261, data.PlayerHeroes[2]);
+            AssertHeroData("HydraSupport_Heal", 542, data.PlayerHeroes[3]);
+            AssertHeroData("RedSkull", 28500, data.PlayerHeroes[4]);
+            AssertHeroData("Blob", 28269, data.OpponentHeroes[0]);
+            AssertHeroData("Magneto", 476, data.OpponentHeroes[1]);
+            AssertHeroData("Juggernaut", 542, data.OpponentHeroes[2]);
+            AssertHeroData("Pyro", 195, data.OpponentHeroes[3]);
+            AssertHeroData("Toad", 261, data.OpponentHeroes[4]);
+        }
+
+        [TestMethod]
         public void GetData_SabretoothIsntBlackBoltInPlayer2()
         {
             BlitzPage page = new();
